49c533028a223969b374c740d4a2cfd5850c1bf9
Khali-repos <Khallrepo@gmail.com>
2025-03-19 19:37:52 +0000

    Backend

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
